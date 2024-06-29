de09ac9afbba979b97da990fb1d70e5f146150b5
Gabriel Martí Capell <electro.gnu@gmail.com>
2024-06-29 19:32:09 +0100

    Initial commit: blog works ;-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'blogApp';
+
+
+
+  // final:
+  // se pueden borrar los TouterOutlet de momento no lo usamos
 }
